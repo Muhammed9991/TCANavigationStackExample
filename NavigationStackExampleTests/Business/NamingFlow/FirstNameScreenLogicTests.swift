36c528f5f93df51e6b7f0de1a98af6ef4d7dffee
Muhammed Mahmood <muhammedmahmood999@gmail.com>
2024-02-13 10:12:44 +0000

    test: FirstNameScreenLogic

--- NavigationStackExampleTests/Business/NamingFlow/FirstNameScreenLogicTests.swift
+++ NavigationStackExampleTests/Business/NamingFlow/FirstNameScreenLogicTests.swift
@@ -5,13 +5,56 @@ import ComposableArchitecture
 @MainActor
 class FirstNameScreenLogicTests: XCTestCase {
     
-    func testDidTapNextButton() async {
+    func testFirstNameBindingEmpty() async {
+        let store = TestStore(initialState: FirstNameScreenLogic.State(firstName: "John", buttonMode: .enabled)) {
+            FirstNameScreenLogic()
+        }
+        
+        await store.send(.binding(.set(\.firstName, ""))) {
+            $0.firstName = ""
+            $0.buttonMode = .disabled
+        }
+    }
+    
+    func testFirstNameBindingHasWhiteSpace() async {
+        let store = TestStore(initialState: FirstNameScreenLogic.State(firstName: "John", buttonMode: .enabled)) {
+            FirstNameScreenLogic()
+        }
+        
+        await store.send(.binding(.set(\.firstName, "  "))) {
+            $0.firstName = "  "
+            $0.buttonMode = .disabled
+        }
+    }
+    
+    func testFirstNameBindingWithText() async {
+        let store = TestStore(initialState: FirstNameScreenLogic.State(buttonMode: .disabled)) {
+            FirstNameScreenLogic()
+        }
+        
+        await store.send(.binding(.set(\.firstName, "John"))) {
+            $0.firstName = "John"
+            $0.buttonMode = .enabled
+        }
+    }
+    
+    func testOnappear() async {
         let store = TestStore(initialState: FirstNameScreenLogic.State()) {
             FirstNameScreenLogic()
         }
         
+        await store.send(.onAppear) {
+            $0.focusedField = .firstName
+        }
+    }
+    
+    func testDidTapNextButton() async {
+        let store = TestStore(initialState: FirstNameScreenLogic.State(firstName: "John")) {
+            FirstNameScreenLogic()
+        }
+        
         await store.send(.didTapNextButton) 
-//        await store.receive(.delegate(.navigateToFamilyNameScreen))
+        await store.receive(.delegate(.navigateToFamilyNameScreen(firstName: "John")))
     }
     
 }
