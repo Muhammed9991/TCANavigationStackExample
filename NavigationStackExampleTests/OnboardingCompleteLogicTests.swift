3186e0ad7ff12541e7773baef997400791072935
Muhammed Mahmood <muhammedmahmood999@gmail.com>
2024-02-13 09:53:19 +0000

    test: OnboardingCompleteLogic

---
+++ NavigationStackExampleTests/OnboardingCompleteLogicTests.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+@testable import NavigationStackExample
+import XCTest
+import ComposableArchitecture
+
+@MainActor
+final class OnboardingCompleteLogicTests: XCTestCase {
+    func testOnAppear() async {
+        let store = TestStore(initialState: OnboardingCompleteLogic.State(dateOfBirth: Date(timeIntervalSince1970: 12345678))) {
+            OnboardingCompleteLogic()
+        } withDependencies: {
+            $0.dataManager = .mock()
+        }
+        
+        await store.send(.onAppear)
+    }
+}
